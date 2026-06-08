77a4615c94ae04eef2f16f40d5fb4b3b91fda17f
ronakmelwani25 <ronakmelwani25@gmail.com>
2026-06-08 19:19:15 +0100

    Add files via upload

--- Birthday Animation.py
+++ Birthday Animation.py
@@ -6,8 +6,16 @@ HEIGHT=1000
 screen= pygame.display.set_mode((WIDTH,HEIGHT))
 screen.fill("white")
 
+#print(pygame.font.get_fonts())
+
 balloon=pygame.image.load("Lesson 2 - Birthday Animation/Images/balloon.jpg")
 bscale=pygame.transform.scale(balloon,(1900,1000))
+font1=pygame.font.SysFont("impact",154)
+text1=font1.render("HAPPY BIRTHDAY",True,"black")
+font2=pygame.font.SysFont("simsun",154)
+text2=font2.render("Have a great day!",True,"navy")
+font3=pygame.font.SysFont("yugothicui",154)
+text3=font3.render("Enjoy your gifts!",True,"red")
 
 cake=pygame.image.load("Lesson 2 - Birthday Animation/Images/cake.jpg")
 cscale=pygame.transform.scale(cake,(1900,1000))
@@ -20,13 +28,16 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
     screen.blit(bscale,(0,0))
+    screen.blit(text1,(500,100))
     pygame.display.update()
     pygame.time.wait(2000)
     screen.fill("white")
     screen.blit(cscale,(0,0))
+    screen.blit(text2,(300,800))
     pygame.display.update()
     pygame.time.wait(2000)
     screen.fill("white")
     screen.blit(pscale,(0,0))
+    screen.blit(text3,(450,800))
     pygame.display.update()
     pygame.time.wait(2000)
